b2603908e8e1e3b6069f33f667c2d7d9552a8b22
VSITE Programiranje <ivan.britvic@vsite.hr>
2014-03-24 21:38:46 +0100

    Predavanje 5
    Događaji i delegati

---
+++ Predavanje5/Predavanje5/Auto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Predavanje5
+{
+    public class Auto:Prometalo
+    {
+        //Dodaj tip delegata
+        public delegate void BoomEventHandler(object sender, EventArgs args);
+        //Evo i događaj
+        public event BoomEventHandler Boom;
+
+        private double _brzina;
+        public string Registracija {get;set;}
+        public double Brzina {
+            get { return _brzina; }
+            set {
+                if (value > 0 && value < 300)
+                    _brzina = value;
+                else
+                    //Kada je brzina veća od 300 explodiraj
+                    onBoom();
+            }
+        }
+        //Procedura koja okida događaj
+        public virtual void onBoom()
+        {   
+            //okini događaj samo ako ima registriranih delegata
+            if (Boom != null)
+                Boom(this, EventArgs.Empty);
+        }
+        //promijeni kod za neku vrijednost
+        public void Ubrzaj(double delta)
+        {
+            Brzina += delta;
+        }
+
+    }
+}
---
+++ Predavanje5/Predavanje5/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Predavanje5
+{
+    public partial class Form1 : Form
+    {
+        Auto a;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double deltaBrzine = Double.Parse(textBox1.Text);
+            //Ubrzaj auto
+            a.Brzina += deltaBrzine;
+
+        }
+        public void auto_Boom(object sender, EventArgs e)
+        {
+            //Malo ružno ljepše bi bio exception...
+            MessageBox.Show("Brzina nije u intervalu", "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Kreiraj novi automobil
+            a = new Auto();
+            // REgistriraj Boom Handler
+            a.Boom += auto_Boom;
+
+        }
+    }
+}
