28b42e9a033f98aaff25d16de298c10b96725785
VSITE Programiranje <ivan.britvic@vsite.hr>
2014-03-31 21:42:05 +0200

    Predavanje 6
    Lambde i Generics

---
+++ Predavanje6/Predavanje6/Auto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Predavanje6
+{
+    public class Auto:Prometalo
+    {
+        //Dodaj tip delegata
+        public delegate void AutoEventHandler(object sender, EventArgs args);
+        //Evo i događaj
+        public event AutoEventHandler Boom;
+        //Evo i lambda događaja
+        public event AutoEventHandler Pozor;
+
+        private double _brzina;
+        public string Registracija {get;set;}
+        public double Brzina {
+            get { return _brzina; }
+            set {
+                if (value > 0 && value < 0.85 * 300)
+                    _brzina = value;
+                else if (value > 0.85 * 300 && value <  300)
+                {
+                    //postavi ga ali okini događaj
+                    _brzina = value;
+                    onPozor();
+                }
+                else
+                    //Kada je brzina veća od 300 explodiraj
+                    onBoom();
+            }
+        }
+        //Procedura koja okida događaj
+        public virtual void onBoom()
+        {   
+            //okini događaj samo ako ima registriranih delegata
+            if (Boom != null)
+                Boom(this, EventArgs.Empty);
+        }
+
+        //Procedura koja okida događaj, pregrijavanje automobila
+        public virtual void onPozor()
+        {
+            //okini događaj samo ako ima registriranih delegata
+            if (Pozor != null)
+                Pozor(this, EventArgs.Empty);
+        }
+        //promijeni kod za neku vrijednost
+        public void Ubrzaj(double delta)
+        {
+            Brzina += delta;
+        }
+
+    }
+}
---
+++ Predavanje6/Predavanje6/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Predavanje6
+{
+    public partial class Form1 : Form
+    {
+        List<Auto> sviAuti = new List<Auto>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Auto a = new Auto();
+            a.Pozor += PorukaPozor;
+            a.Boom += (o, args) => { MessageBox.Show("Auto Boom"); };
+            a.vlasnik = Vlasnistvo.Tvrtka;
+            a.Registracija = tb_reg.Text;
+            a.Brzina = Double.Parse(tb_brzina.Text);
+            sviAuti.Add(a);
+        }
+        private void PorukaPozor(object o, EventArgs args)
+        {
+            Auto aut1 = (Auto) o;
+            //Pokaži poruku o pregrijavanju
+            MessageBox.Show("Auto ide prebrzo: " + aut1.Brzina.ToString());
+        }
+    }
+}
