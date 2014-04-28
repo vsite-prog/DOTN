b6653b8340d338cfb2ee966eb6b21d5019ebe0ae
VSITE Programiranje <ivan.britvic@vsite.hr>
2014-04-28 21:30:58 +0200

    Predavanje 8
    GridView i LINQ

---
+++ Predavanje8/Predavanje8/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Predavanje8
+{
+    public partial class Form1 : Form
+    {
+        //Neka bude global
+        BazaEntities db;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            db = new BazaEntities();
+            var klad = (from p in db.parovi
+                         select p.klad_id)
+                        .Distinct();
+            cb_kladionica.DataSource = klad.ToList();
+            /*
+            var kladionice = from k in db.parovi
+                             select new { Naziv = k.naziv, Koeficijent = k.koef };
+            dataGridView1.DataSource = kladionice.ToList();
+             * */
+        }
+
+        private void cb_kladionica_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int brojKladionice = (int)cb_kladionica.SelectedValue;
+            /*var kladionice = from k in db.parovi
+                             where k.klad_id == brojKladionice
+                             select new { Naziv = k.naziv, Koeficijent = k.koef };
+             * */
+            var kladionice = db.parovi.Where(p => p.klad_id == brojKladionice);
+            dataGridView1.DataSource = kladionice.ToList();
+        }
+    }
+}
