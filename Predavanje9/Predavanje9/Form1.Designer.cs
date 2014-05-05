fd791fa5ee75a251841ddff35e4b18f62263e907
VSITE Programiranje <ivan.britvic@vsite.hr>
2014-05-05 21:38:23 +0200

    Predavanje 9 - Mreža i Threading
    WebBrowser  komponenta i pokretanje novog thread-a

---
+++ Predavanje9/Predavanje9/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Predavanje9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_adresa = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Web adresa:";
+            // 
+            // tb_adresa
+            // 
+            this.tb_adresa.Location = new System.Drawing.Point(143, 13);
+            this.tb_adresa.Name = "tb_adresa";
+            this.tb_adresa.Size = new System.Drawing.Size(252, 20);
+            this.tb_adresa.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(411, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Otvori";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 57);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(625, 405);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            this.webBrowser1.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.webBrowser1_Navigating);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 474);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb_adresa);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Preglednik";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_adresa;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Predavanje9/Predavanje9/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Predavanje9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //Trenutna nit koja se izvodi je glavna
+            Thread.CurrentThread.Name = "Glavna";
+            //idemo je izvesti  asinhrono
+            Thread novaNit = new Thread(new ThreadStart(nista_pametno));
+            novaNit.Name = "Nova";
+            nista_pametno();
+
+            //novaNit.Name = "Nova2"; ime se postavljasamo na početku
+            //sad zbilja pokreni izvođenje
+            novaNit.Start();
+            //sinhrono
+            nista_pametno();
+            //MessageBox.Show(Thread.CurrentThread.Name);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Uri.IsWellFormedUriString(tb_adresa.Text, UriKind.Absolute))
+                webBrowser1.Navigate(tb_adresa.Text);
+            else
+                MessageBox.Show("Kakva je to adresa???","Gr4eska");
+        }
+
+        private void webBrowser1_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            label2.Text = "Ima li mreže? skidam stranicu...";
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            label2.Text = "Gotovo";
+        }
+        private void nista_pametno()
+        {
+            Thread.Sleep(2000);
+            // MessageBox.Show(Thread.CurrentThread.Name);
+            label2.Text = Thread.CurrentThread.Name;
+        }
+    }
+}
