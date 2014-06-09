93f7a3d71355954bf6f82be6ab7b1bb07f11ea1e
VSITE Programiranje <ivan.britvic@vsite.hr>
2014-06-09 21:43:03 +0200

    Predavanje 14 WPF komande. stilovi i animacije

---
+++ Predavanje14/Predavanje14/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace Predavanje14
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            tb1.Text = "Naš text1 \n u vuiše linija...";
+            tb2.Text = "Naš text2 \n u vuiše linija...";
+            tb3.Text = "Naš text3 \n u vuiše linija...";
+            
+        }
+
+        private void New_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("New action");
+        }
+        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == true)
+                MessageBox.Show("Odabrana datoteka");
+        }
+
+        private void Open_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+           // e.CanExecute = false;
+            e.CanExecute = true;
+        }
+        private void New_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+    }
+}
