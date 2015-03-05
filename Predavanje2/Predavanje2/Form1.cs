93841a9fd2571708f6c1f7922e84e96590bf1f05
VSITE Programiranje <ivan.britvic@vsite.hr>
2015-03-05 18:09:01 +0100

    Windows Forme
    Windows Forme, otvaranje, zatvaranje, MesaggeBox dialog

---
+++ Predavanje2/Predavanje2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Predavanje2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dobar dan", "Poruka", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Kreiramo novu formu ali se još ne vidi
+            Form drugaForma = new Form2();
+            //Pokreni paralelno
+            //drugaForma.Show();
+
+           DialogResult dr =  drugaForma.ShowDialog();
+            //Vidi što je vraćeno
+           if (dr == System.Windows.Forms.DialogResult.OK)
+               MessageBox.Show("Vraćeno je OK");
+           else
+               MessageBox.Show("Vraćeno je Cacel");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Application.Exit();
+
+            //Zatvora formu
+            this.Close();
+        }
+    }
+}
---
+++ Predavanje2/Predavanje2/Form2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Predavanje2
+{
+    public partial class Form2 : Form
+    {
+        int brojac;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            brojac++;
+            label1.Text = "kLIKNULI STE " + brojac.ToString() + ". put!";
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Kliknuli ste 0. puta!";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Zatvori formu i vrato odgovarajući rezult.
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Zatvori formu i vrato odgovarajući rezult.
+            this.DialogResult = DialogResult.Cancel;
+        }
+    }
+}
