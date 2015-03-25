a368a7fc34ef149e9ce7cf3846589df3b8b37398
VSITE Programiranje <ivan.britvic@vsite.hr>
2015-03-25 18:35:18 +0100

    Predavanje 5 - Events
    Događaji, delegati...

---
+++ Predavanje5/Predavanje5/Auto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Predavanje5
+{
+    class Auto
+    {
+        public delegate void Explodiraj(double v);
+        //Prebrzo sve eksplodiralo
+        public event Explodiraj boom;
+        //svojstvo brzine
+        private double _brzina;
+        public double Brzina { 
+            get {return _brzina;}
+            set {
+                if (value < 0)
+                {
+                    _brzina = 0;
+                }
+                else if (value < 220)
+                {
+                    _brzina = value;
+                }
+                else
+                {
+                    onBoom(value);
+                }
+            
+            }
+        }
+
+        //Okini događaj
+        protected virtual void onBoom(double brzina)
+        {
+            if (boom != null) //Provjeri je li ima preplaćenih objekata
+                boom(brzina);
+
+        }
+    }
+}
---
+++ Predavanje5/Predavanje5/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Predavanje5
+{
+    public partial class Form1 : Form
+    {
+        Auto a;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            a = new Auto();
+            //Pretplati me na boom događaj
+          /*  ne mogu samo ja imati pravo na događaj 
+           * a.boom = promijeniBrzinu;
+            a.boom = new Auto.Explodiraj(promijeniBrzinu);
+           */
+           a.boom += EksplodirajHandler;
+           
+
+            tb_brzina.Text = "0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            promijeniBrzinu(26.3);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            promijeniBrzinu(-25.5);
+
+        }
+
+        void promijeniBrzinu(double delta)
+        {
+            a.Brzina += delta;
+            //Prikaži brzinu
+            tb_brzina.Text = a.Brzina.ToString();
+        }
+
+
+        //Ovo je naš event handler za auto, idemo ga iskoristiti
+        void EksplodirajHandler(double brzina)
+        {
+            MessageBox.Show("Boom išli ste čak: " + brzina.ToString() + " na sat!", "Eksplzija", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+        }
+    }
+}
