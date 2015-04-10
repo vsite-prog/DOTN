7b473271d5a7dd1f8e8492e2afabc43ca80d3ee0
VSITE Programiranje <ivan.britvic@vsite.hr>
2015-04-10 17:06:51 +0200

    Predavanje 6
    Lambda funkcije i generici

---
+++ Predavanje6/Predavanje5/Auto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Predavanje5
+{
+    class Auto
+    {
+        public delegate void Explodiraj(double v);
+        //Prebrzo sve eksplodiralo
+        public event Explodiraj boom;
+
+        public event EventHandler pozor; 
+        //svojstvo brzine
+        private double _brzina;
+        public double Brzina { 
+            get {return _brzina;}
+            set {
+                if (value < 0)
+                {
+                    _brzina = 0;
+                }
+                else if (value < 200)
+                {
+                    _brzina = value;
+                }
+                else if (value < 240)
+                {
+                    _brzina = value;
+                    onPozor();
+                }
+                else
+                {
+                    onBoom(value);
+                }
+            
+            }
+        }
+
+        //Okini događaj
+        protected virtual void onBoom(double brzina)
+        {
+            if (boom != null) //Provjeri je li ima preplaćenih objekata
+                boom(brzina);
+
+        }
+
+        //Okini događaj
+        protected virtual void onPozor()
+        {
+            if (pozor != null) //Provjeri je li ima preplaćenih objekata
+                pozor(this, EventArgs.Empty);
+
+        }
+    }
+}
---
+++ Predavanje6/Predavanje5/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Predavanje5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_brzina = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lb_status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Brzina automobila:";
+            // 
+            // tb_brzina
+            // 
+            this.tb_brzina.Location = new System.Drawing.Point(144, 41);
+            this.tb_brzina.Name = "tb_brzina";
+            this.tb_brzina.ReadOnly = true;
+            this.tb_brzina.Size = new System.Drawing.Size(115, 20);
+            this.tb_brzina.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(265, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(36, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            //this.button1.Click += this.button1_Click;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(307, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(36, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lb_status
+            // 
+            this.lb_status.AutoSize = true;
+            this.lb_status.Location = new System.Drawing.Point(2, 4);
+            this.lb_status.Name = "lb_status";
+            this.lb_status.Size = new System.Drawing.Size(0, 13);
+            this.lb_status.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 136);
+            this.Controls.Add(this.lb_status);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb_brzina);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Auto";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_brzina;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lb_status;
+    }
+}
+
