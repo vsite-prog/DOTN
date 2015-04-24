6d000022d57c1b88ed04b43a07fbb0098c35fe04
VSITE Programiranje <ivan.britvic@vsite.hr>
2015-04-24 17:01:15 +0200

    Predavanje 8
    Entity framework i LINQ

---
+++ Predavanje8/Pred8/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Pred8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_naziv = new System.Windows.Forms.TextBox();
+            this.tb_koeficijent = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cb_kladionica = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 340);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Unesi par:";
+            // 
+            // tb_naziv
+            // 
+            this.tb_naziv.Location = new System.Drawing.Point(142, 340);
+            this.tb_naziv.Name = "tb_naziv";
+            this.tb_naziv.Size = new System.Drawing.Size(202, 20);
+            this.tb_naziv.TabIndex = 1;
+            // 
+            // tb_koeficijent
+            // 
+            this.tb_koeficijent.Location = new System.Drawing.Point(142, 382);
+            this.tb_koeficijent.Name = "tb_koeficijent";
+            this.tb_koeficijent.Size = new System.Drawing.Size(202, 20);
+            this.tb_koeficijent.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 385);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "UNesi koeficijent:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Odaberi kladionicu:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 427);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Unesi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cb_kladionica
+            // 
+            this.cb_kladionica.FormattingEnabled = true;
+            this.cb_kladionica.Location = new System.Drawing.Point(140, 12);
+            this.cb_kladionica.Name = "cb_kladionica";
+            this.cb_kladionica.Size = new System.Drawing.Size(204, 21);
+            this.cb_kladionica.TabIndex = 6;
+            this.cb_kladionica.SelectedIndexChanged += new System.EventHandler(this.cb_kladionica_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(56, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(428, 234);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 490);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cb_kladionica);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_koeficijent);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_naziv);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Kladionice parovi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_naziv;
+        private System.Windows.Forms.TextBox tb_koeficijent;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cb_kladionica;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Predavanje8/Pred8/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pred8
+{
+    public partial class Form1 : Form
+    {
+        KladjenjeEntities db = new KladjenjeEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Poveži kombo s tablicom kladionica iz baze
+            cb_kladionica.ValueMember = "id";
+            cb_kladionica.DisplayMember = "naziv";
+            cb_kladionica.DataSource = db.Kladionica.ToList<Kladionica>();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Kreiran jw novi objekt tipa KLadionica (iz EF-a)
+            Par par = new Par();
+            par.naziv = tb_naziv.Text;
+            par.koeficijent = Double.Parse(tb_koeficijent.Text);
+            par.Kladionica = (Kladionica)cb_kladionica.SelectedItem;
+
+            //Dodaj par u kolekciju parova, još nije spremljeno
+            db.Par.Add(par);
+
+            //Sinhroniziraj
+            db.SaveChanges();
+
+        }
+
+        private void cb_kladionica_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int kladionica = (int)cb_kladionica.SelectedValue;
+            // dataGridView1.DataSource = db.Par.Where(p => p.kladId == kladionica);
+            dataGridView1.DataSource = (
+                                            from par in db.Par
+                                            where par.kladId == kladionica
+                                            select new { Naziv = par.naziv, Koeficijent = par.koeficijent }
+                                        ).ToList();
+        }
+    }
+    
+}
