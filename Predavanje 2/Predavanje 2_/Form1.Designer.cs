4b349d54f98979b98ebe012abdb7ded73c100964
vsite-prog <ivan.britvic@vsite.hr>
2017-03-10 14:33:50 +0100

    Windows forme
    Windows forme

---
+++ Predavanje 2/Predavanje 2_/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Predavanje_2_
+{
+    partial class Prva
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_unos = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.glavniToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.drugaFormaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.izlazToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(302, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "KLikni me";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Upiši nešto:";
+            // 
+            // tb_unos
+            // 
+            this.tb_unos.Location = new System.Drawing.Point(80, 58);
+            this.tb_unos.Name = "tb_unos";
+            this.tb_unos.Size = new System.Drawing.Size(216, 20);
+            this.tb_unos.TabIndex = 2;
+            this.tb_unos.Text = "Isada";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.glavniToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(401, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // glavniToolStripMenuItem
+            // 
+            this.glavniToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.drugaFormaToolStripMenuItem,
+            this.izlazToolStripMenuItem});
+            this.glavniToolStripMenuItem.Name = "glavniToolStripMenuItem";
+            this.glavniToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.glavniToolStripMenuItem.Text = "Glavni";
+            // 
+            // drugaFormaToolStripMenuItem
+            // 
+            this.drugaFormaToolStripMenuItem.Name = "drugaFormaToolStripMenuItem";
+            this.drugaFormaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.drugaFormaToolStripMenuItem.Text = "Druga forma";
+            this.drugaFormaToolStripMenuItem.Click += new System.EventHandler(this.drugaFormaToolStripMenuItem_Click);
+            // 
+            // izlazToolStripMenuItem
+            // 
+            this.izlazToolStripMenuItem.Name = "izlazToolStripMenuItem";
+            this.izlazToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.izlazToolStripMenuItem.Text = "Izlaz";
+            this.izlazToolStripMenuItem.Click += new System.EventHandler(this.izlazToolStripMenuItem_Click);
+            // 
+            // Prva
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 129);
+            this.Controls.Add(this.tb_unos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Prva";
+            this.Text = "Prva forma";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_unos;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem glavniToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem drugaFormaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem izlazToolStripMenuItem;
+    }
+}
+
---
+++ Predavanje 2/Predavanje 2_/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Predavanje_2_
+{
+    public partial class Prva : Form
+    {
+        public Prva()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string txt = tb_unos.Text;
+            //Idemo ovo prikazati korisniku
+            MessageBox.Show(txt);
+        }
+
+        private void drugaFormaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Kreiraj novu formu
+            Druga forma = new Druga();
+            //Ne vidi se još, idemo je prikazati
+            forma.Show();
+        }
+
+        private void izlazToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
