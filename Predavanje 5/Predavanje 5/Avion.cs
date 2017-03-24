9dd05a1302560ec322b9fa4bc1b1c1effe45e4a9
vsite-prog <ivan.britvic@vsite.hr>
2017-03-24 17:03:12 +0100

    Delegati, događaji
    Delegati, događaji

---
+++ Predavanje 5/Predavanje 5/Avion.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Predavanje_5
+{
+    //Tip delegata
+    public delegate void IspisDelegate(string txt);
+    public class Avion
+    {
+        //Tip funkcije koja reagira događaj
+        public delegate void EventDelegate(double v);
+        //Ovo je event emitter definiraj događaj
+        public event EventDelegate crash;
+
+        public Avion(string naziv, double brzina)
+        {
+            Naziv = naziv;
+            this.Brzina = brzina;
+        }
+        double _v;
+        public string Naziv
+        {
+            get;
+            set;
+        }
+        public double Brzina
+        {
+            get { return _v; }
+
+            set
+            {
+                if (value < 0 || value > 1200){
+                    //okini događaj
+                    onCrash(value); //ovo je funkcija koja će okinuti događaj
+                } else {
+                    _v = value;
+                }
+            }
+        }
+
+        //ja okidam događaj
+        void onCrash (double v)
+        {
+            //Provjeri da li je itko se pretplatio na događaj
+            if (crash != null){
+                crash(v);
+            }
+        }
+
+        //Definirati funkciju koja ispisuje a nema pojma kako
+        public void Ispisi (IspisDelegate f)
+        {
+            string txt = "Avion: " + Naziv + " brzina: " + Brzina.ToString();
+            f(txt);
+        }
+    }
+}
---
+++ Predavanje 5/Predavanje 5/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Predavanje_5
+{
+
+    public partial class Form1 : Form
+    {
+        //Globalna Varijabla
+        Avion avion;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            avion = new Avion("Strelko", 679.5);
+            //registriraj me na događaj
+            avion.crash += CrashPoruka;
+        }
+
+        //Ovo je event handler, procedura događaja
+        void CrashPoruka( double brzina)
+        {
+            MessageBox.Show("Crash, brzina je trebala biti: " + brzina.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IspisDelegate del1 = mbox;
+            //ili s lambdom
+            del1 = txt => { MessageBox.Show(txt); };
+            //MessageBox ispis
+            avion.Ispisi(del1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            IspisDelegate del1 = labela;
+            //ili
+            del1 = txt => { label1.Text = txt; };
+            //Label ispis
+            avion.Ispisi(del1);
+        }
+
+        //naša lokalna funkcija
+        void labela(string txt)
+        {
+            label1.Text = txt;
+        }
+
+        void mbox(string txt)
+        {
+            MessageBox.Show(txt);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Multicasting
+            IspisDelegate del1 = labela;
+            del1 += txt => { MessageBox.Show(txt); };
+            //Isppisi sve
+            avion.Ispisi(del1);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            avion.Brzina += Double.Parse(textBox1.Text);
+        }
+    }
+}
