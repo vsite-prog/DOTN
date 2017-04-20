4e8adb24acc9410b5bfd35cfaef4790f877f9967
vsite-prog <ivan.britvic@vsite.hr>
2017-04-20 15:29:54 +0200

    Rad sa bazama, Entity Framework
    Rad sa bazama, Entity Framework

---
+++ Predavanje 8/Predavanje 8/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Predavanje_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb_vrsta = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_pas = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dgv_psi = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_psi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cb_vrsta
+            // 
+            this.cb_vrsta.FormattingEnabled = true;
+            this.cb_vrsta.Location = new System.Drawing.Point(125, 36);
+            this.cb_vrsta.Name = "cb_vrsta";
+            this.cb_vrsta.Size = new System.Drawing.Size(184, 21);
+            this.cb_vrsta.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Vrsta psa:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(345, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Naziv psa:";
+            // 
+            // tb_pas
+            // 
+            this.tb_pas.Location = new System.Drawing.Point(422, 33);
+            this.tb_pas.Name = "tb_pas";
+            this.tb_pas.Size = new System.Drawing.Size(174, 20);
+            this.tb_pas.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(422, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Spremi novog psa";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgv_psi
+            // 
+            this.dgv_psi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_psi.Location = new System.Drawing.Point(49, 148);
+            this.dgv_psi.Name = "dgv_psi";
+            this.dgv_psi.Size = new System.Drawing.Size(547, 243);
+            this.dgv_psi.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 403);
+            this.Controls.Add(this.dgv_psi);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb_pas);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cb_vrsta);
+            this.Name = "Form1";
+            this.Text = "Psi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_psi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb_vrsta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_pas;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgv_psi;
+    }
+}
+
---
+++ Predavanje 8/Predavanje 8/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Predavanje_8
+{
+    public partial class Form1 : Form
+    {
+        //daj mi ručku na bazu da mogu raditi s njom
+        PsiEntities baza = new PsiEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Evo što se prikazuje a evo što se pamti
+            cb_vrsta.DisplayMember = "Naziv";
+            cb_vrsta.ValueMember = "id";
+            //Neka ti tablica iz baze bude izvor podataka
+            cb_vrsta.DataSource = baza.Vrsta.ToList<Vrsta>();
+
+            //Prikaži pse u GView-u
+            prikaziPse();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Kreiraj novog psa prema zahtjevu
+            Pas pas = new Pas();
+            //Postavi naziv iz TB-a
+            pas.Naziv = tb_pas.Text;
+            //Vidi u Combo kojoj vrsti pripada
+            pas.Vrsta = (Vrsta)cb_vrsta.SelectedItem;
+            //Ovo još nije stavljeno u tablicu
+            baza.Pas.Add(pas);
+            //Još nije spremljeno u bazu, idemo
+            baza.SaveChanges();
+            //Prikaži pse
+            prikaziPse();
+
+        }
+
+        void prikaziPse()
+        {
+            dgv_psi.DataSource = baza.Pas.ToList<Pas>();
+        }
+    }
+}
