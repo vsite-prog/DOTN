18b6c5c14d87c836b6fef1af7666c2345865d1c5
vsite-prog <ivan.britvic@vsite.hr>
2018-03-23 20:24:36 +0100

    Delegati, lambde, eventi
    Delegati, lambde, eventi

---
+++ Predavanje 5/Predavanje 5/Brod.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Predavanje_5
+{
+    //Ovo je delegat koji definira tip funkcije koja se zove kad se desi događaj
+    public delegate void PotopDelegat(string txt);
+    class Brod
+    {
+       double _ukrcano;
+       //Ovdje smo definirali naš događaj 
+       public event PotopDelegat Potop;
+       public string Naziv { get; set; }
+       public double Nosivost {
+            get;set;
+        } //Max težina tereta
+       public double Ukrcano
+        {
+            get
+            {
+                return _ukrcano;
+            }
+            set
+            {
+                //Vidi da li je prekoračeno
+                if (value <= Nosivost)
+                    _ukrcano = value;
+                else //Ovo nije idealno zbog exception-a, objašnjenje ...
+                    Potop(Naziv + ": potoopljen prekoračena nosivost: " + value.ToString());
+            }
+        }//Ukrcana težina tereta
+
+        public Brod(string pNaziv, double pNosivost)
+        {
+            Naziv = pNaziv;
+            Nosivost = pNosivost;
+        }
+
+    }
+}
---
+++ Predavanje 5/Predavanje 5/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Predavanje_5
+{
+    //Ovo je isto tip kao i klasa
+    delegate void PrintDelegate(string s);
+
+    public partial class Form1 : Form
+    {
+        Brod b;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // printaj(tb_unos.Text); Idemo ovo predstaviti kroz delegate
+            PrintDelegate d = new PrintDelegate(pisiULabelu);
+             d +=  txt => MessageBox.Show("Poruka :" + txt, "Print", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            printaj(d);
+
+        }
+
+        void printaj(PrintDelegate d) //Pazite ovdje ide tip delegate
+        {
+            //Izvrši dobivenu funkciju
+            d(tb_unos.Text);          
+        }
+
+        void pisiULabelu(string txt)
+        {
+            lb_print.Text = txt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Novi brod
+            b = new Brod("Nema imena", 1000);
+            // Ajde pretplati se na događaj potopa
+            b.Potop += txt => MessageBox.Show("Poruka :" + txt, "Print", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            b.Ukrcano += Double.Parse(tb_ukrcaj.Text);
+        }
+    }
+}
