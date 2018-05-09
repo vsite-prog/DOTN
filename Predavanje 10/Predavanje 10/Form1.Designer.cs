bfa8b55867262eca2fe64664d44e146392e3ecab
vsite-prog <ivan.britvic@vsite.hr>
2018-05-09 16:33:16 +0200

    Niti, Backgroundworker
    Niti, Backgroundworker

---
+++ Predavanje 10/Predavanje 10/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Predavanje_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Piši";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(188, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Piši BWorker";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(392, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 543);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Predavanje 10/Predavanje 10/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Predavanje_10
+{
+
+   public partial class Form1 : Form
+    {
+        Spremnik spremnik = new Spremnik();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+
+            //Idemo u drugi thread
+            ThreadStart delegat = new ThreadStart(pisi);
+            Thread thread1 = new Thread(delegat);
+            thread1.Name = "Nit 2";
+            thread1.Start();
+
+            ThreadStart delegat2 = pisi; //kraci način pisanja
+            Thread thread2 = new Thread(pisi); //ovo je još i kraći ovo gore nam ne treba
+            thread2.Name = "Nit 3"; //Ovo se postavlja samo jednom
+            thread2.Start();
+
+
+            //Prije UI niti malo pričekaj
+            Thread.Sleep(2000);
+
+            //Ovo je UI thread
+            pisi();
+        }
+
+        void pisi()
+        {
+            //Ova procedura u biti mijenja zajednički resurs+
+            for (int i=0; i< 10; i++)
+            {
+                lock (spremnik) //Ne daj drugim nitima da rade dok ja nisam gotov s prolazom
+                {
+                    spremnik.Broj += 2;
+                    //Idemo malo usporiti
+                    Thread.Sleep(1000); //u milisekundama
+                    label1.Text += "\nProlaz: " + spremnik.Broj.ToString() + " " + Thread.CurrentThread.Name;
+                } 
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //ovo je UI nit
+            Thread glavnaNit = Thread.CurrentThread;
+            glavnaNit.Name = "Glavna nit";
+
+            //Neka javlja kad je prešao neki milestone
+            backgroundWorker1.WorkerReportsProgress = true;
+
+            backgroundWorker1.DoWork += (o, eArgs) =>
+            {
+                //Sada smo u drugoj niti
+                //Ova nit nešto radi, mi ćemo kreirati text da lsuži kao prmjer
+                StringBuilder builder = new StringBuilder();
+                builder.Append("Druga nit:");
+                for (int i=1; i<11; i++)
+                {
+                    Thread.Sleep(1000); //jedna sekunda pauze
+                    builder.Append("\nProlaz: ");
+                    builder.Append(i);
+                    builder.Append(" vrijeme: ");
+                    builder.Append(DateTime.Now.ToLongTimeString());
+                    backgroundWorker1.ReportProgress(i * 10);
+                }
+                eArgs.Result = builder.ToString(); //Vrati nam text koji si napravio
+            };
+
+            backgroundWorker1.RunWorkerCompleted += (o, eArgs) =>
+            {
+                //OPet smo nazad u UI niti
+                label1.Text = eArgs.Result.ToString();
+            };
+
+            backgroundWorker1.ProgressChanged += (o, eArgs) =>
+            {
+                //Kao i kod završetka, ovdje smo se vratili u UI nit
+                label2.Text = "Završeno  " + eArgs.ProgressPercentage.ToString() + "% !";
+            };
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Kreni s izvođenjem
+            backgroundWorker1.RunWorkerAsync();
+        }
+    }
+
+    public class Spremnik
+    {
+        public int Broj { get; set; }
+    }
+}
