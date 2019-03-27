da9db81f4b6dd5d2a09e699884a2a012fcd97717
vsite-prog <ivan.britvic@vsite.hr>
2019-03-27 17:19:26 +0100

    Događaji, generički tipovi
    Događaji, generički tipovi

---
+++ Predavanje 6/Predavanje 6/Avion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Predavanje_6
+{
+    class Avion
+    {
+
+        public Avion(string n, double v)
+        {
+            Naziv = n;
+            Brzina = v;
+        }
+        public event EventHandler Boooooom;
+
+        public string Naziv { get; set; }
+
+        double _brzina; // skribvena varijabla svojstva
+        public double Brzina
+        {
+            get => _brzina; // Sintaksa slična labmdi za svojstva i metode
+            set
+            {
+                // E sad ću da eksplodiram ako brzina nije u redu
+                if( value > 1200 || value < 0)
+                {
+                    // Okinuti događaj
+                    // Boooooom(this, EventArgs.Empty); ali ne ovako direktno
+                    onBoooom(); // napisati ćemo sve isto ali sada jednostavnije 
+                    Boooooom?.Invoke(this, EventArgs.Empty); // Drugi i bolji način dizanja događaja
+                } else
+                {
+                    _brzina = value;
+                }
+            }
+        }
+
+        protected virtual void onBoooom()
+        {
+            if (Boooooom != null) // provjerava da li se iko prijavio ne njega
+            {
+                Boooooom(this, EventArgs.Empty);
+            }
+        }
+
+        public override string ToString() // sa override K.riječi nema problema
+        {
+            return String.Format("Avion: {0}, brzina: {1}", this.Naziv, Brzina);
+        }
+    }
+}
---
+++ Predavanje 6/Predavanje 6/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Predavanje_6
+{
+    public partial class Form1 : Form
+    {
+        Avion avion = new Avion("Letimiš", 100);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            poruka<string>("Kliknut");
+            // Bi li to radilo sa broijevim
+            poruka<double>(12.4);
+            poruka<int>(14);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Kreiraj novo dugme
+            Button button = new Button();
+            button.Location = new System.Drawing.Point(180, 29);
+            button.Name = "button1";
+            button.Size = new System.Drawing.Size(90, 23);
+            button.TabIndex = 0;
+            button.Text = "Klikni i mene";
+            button.UseVisualStyleBackColor = true;
+
+            // Dodaj u formu 
+            this.Controls.Add(button);
+
+            // Neka nešto radi na klik
+            button.Click += button1_Click;
+
+            // Idemo se odjaviti
+            button.Click -= button1_Click;
+
+            // Idemo reagirati s Lambdom
+            button.Click += (o, args) =>
+            {
+                MessageBox.Show("Lambda Kliknut");
+
+                // Za svako klikanje dodaj k u tekst
+                ( (Button)o).Text += "k";
+
+            };
+
+            // Ovome ćemmo dodati dinamički
+            button3.Click += (o, args) => { avion.Brzina -= 100; };
+
+            // Idemo nešto napraviti sa događajem boooma
+            avion.Boooooom += (o, args) =>
+            {
+                Avion a = (Avion)o;
+                poruka<Avion>(a);
+            };
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            avion.Brzina += 100;
+        }
+
+        // Generička funkcija
+        void poruka<T>(T objekt)
+        {
+            MessageBox.Show(objekt.ToString(), "Poruka", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+    }
+}
