2ddf2515af29cb4b8b60d21147541a3633e5c242
Ivan Britvic <ivbritvic@gmail.com>
2021-05-06 11:46:01 +0200

    Threads, BackgroundWorker
    Threads, BackgroundWorker

---
+++ Predavanje 10/Predavanje 10/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace Predavanje_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Pokreni niti";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(204, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Pokreni pozadinca";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(-4, 526);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(967, 46);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(962, 572);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Predavanje 10/Predavanje 10/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Predavanje_10
+{
+    public partial class Form1 : Form
+    {
+        Rampa rampa = new Rampa(); // Referentni tip za zaključavanje
+        public Form1()
+        {
+            InitializeComponent();
+            Thread thread = Thread.CurrentThread; // Ovo je naša glavna nit
+            //Idemo joj promijeniti ime
+            thread.Name = "Glavna/UI nit";
+            // label1.Text = Thread.CurrentThread.Name;
+
+       
+        }
+
+        private void Radnik_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Radnik_DoWork(object sender, DoWorkEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Thread novaNit = new Thread(); // TRaži nekakav kod, funkciju koja će se izvoditi
+            ThreadStart delegatNiti = radiPoNiti; // Deleget za thread 
+            /*
+            Thread drugaNit = new Thread(radiPoNiti);
+            drugaNit.Name = "Druga nit";
+            drugaNit.Start(); // Izvrši nit i izađi kad si gotov
+            */
+
+            for (int i = 0; i < 4; i++)
+            {
+                Thread thread = new Thread(radiPoNiti); // new ThreadStart(radiPoNiti)
+                thread.Name = " Nit " + (i + 1).ToString();
+                thread.Priority = ThreadPriority.Lowest;
+                if (i == 2) thread.Priority = ThreadPriority.Highest; // Možemo i sami postaviti prioritet
+                thread.Start();
+            }
+
+
+            radiPoNiti();
+        }
+
+        void radiPoNiti()
+        {
+            lock (rampa) // Došao si do rampe, sada prođi, neka te drugi čekaji
+            {
+                rampa.Prolaz++; // Još jedan prolaz kroz rampu
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(1000); // 1 sekunda, ne radi ništa
+                    label1.Text += $"\n{Thread.CurrentThread.Name} rampa prolaz: {rampa.Prolaz}, prolaz petlje broj: {i + 1} - {DateTime.Now.ToLongTimeString()} ";
+                }
+                //label1.Text += "\n" + Thread.CurrentThread.Name;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            BackgroundWorker radnik = new BackgroundWorker();
+            // Ovo je drugi thread
+            radnik.DoWork += (o, a) =>
+            {
+                StringBuilder graditelj = new StringBuilder(); // Kako bi biloi brže, umjesto stringa koristiti ću ovu komponento
+                graditelj.AppendLine("Idemooo ");
+                for (int i = 0; i < 10; i++)
+                {
+                    Thread.Sleep(1000); // Čekaj sekundu
+                    graditelj.Append(" Prolaz broj: ");
+                    graditelj.Append(i + 1);
+                    graditelj.Append(" Vrijeme: ");
+                    graditelj.Append(DateTime.Now.ToLongTimeString());
+                    graditelj.Append("\n");
+                    radnik.ReportProgress((i + 1) * 10);
+                }
+                graditelj.AppendLine("Kraj, the end! ");
+                // Vratiti ćemo podatke u glavnu nit
+                a.Result = graditelj.ToString();
+            };
+            // Ovo je glavni thread
+            radnik.RunWorkerCompleted += (o, a) =>
+            {
+                // Threadsafe iz druge niti tekst
+                label1.Text = a.Result.ToString();
+            };
+
+            radnik.ProgressChanged += (o, a)=>{
+                progressBar1.Value = a.ProgressPercentage;
+            };
+            radnik.WorkerReportsProgress = true;
+            radnik.RunWorkerAsync(); // Izvršavaj se u drugoj niti
+        }
+    }
+    class Rampa
+    {
+        public int Prolaz { get; set; }
+    }
+}
