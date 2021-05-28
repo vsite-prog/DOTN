075829bd8ca36d9ddceb1f08b51bc76c7a8021d5
Ivan Britvic <ivbritvic@gmail.com>
2021-05-28 10:11:44 +0200

    Primjer WPF aplikacije
    Primjer WPF aplikacije

---
+++ WPFAplikacija/WPFAplikacija/DrugiProzor.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFAplikacija
+{
+    /// <summary>
+    /// Interaction logic for DrugiProzor.xaml
+    /// </summary>
+    public partial class DrugiProzor : Window
+    {
+        public DrugiProzor()
+        {
+            InitializeComponent();
+            // DialogResult = true; // Samo za test, mora biti na dugmetu za zatvaranje
+        }
+    }
+}
---
+++ WPFAplikacija/WPFAplikacija/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFAplikacija
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Isto kao u formsima i funkcionira
+            MessageBox.Show("Kliknuli ste na Dugme!", "Naslov", MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+        }
+
+        private void Meni_Drugi(object sender, RoutedEventArgs e)
+        {
+            DrugiProzor prozor = new DrugiProzor();
+            bool? izlaz = prozor.ShowDialog();
+            string poruka = izlaz ?? false ? "Kliknuli ste OK" : "Kliknuli ste Cancel"; // ?? znače kad je vrijednust null prebaci je u false
+            MessageBox.Show(poruka, "Naslov", MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+        }
+        private void Meni_Izlaz(object sender, RoutedEventArgs e)
+        {
+            // Isto kao u formsima i funkcionira
+            this.Close();
+        }
+    }
+}
