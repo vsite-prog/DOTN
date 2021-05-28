dafda19d4f9cc7347805f346f23eb2fb6f21a131
Ivan Britvic <ivbritvic@gmail.com>
2021-05-28 10:10:57 +0200

    REST Servis
    REST Servis

---
+++ PorukaServis/PorukaServis/IPoruka.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PorukaServis
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IPoruka
+    {
+        // Po id-u daj mi jednu poruku
+        [WebGet(ResponseFormat = WebMessageFormat.Xml, UriTemplate = "/poruka/{id}")]
+        [OperationContract]
+        Poruka JednaPoruka(string id);
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/poruke")]
+        List<Poruka> SvePoruke();
+
+    }
+
+   [DataContract]
+    public class Poruka
+    {
+        // Konstruktor koji nije vidljiv za vanjske klijente
+        public Poruka(int id, string salje, string prima, string tekst)
+        {
+            Id = id;
+            Prima = prima;
+            Salje = salje;
+            Tekst = tekst;
+        }
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Salje { get; set; }
+
+        [DataMember]
+        public string Prima { get; set; }
+
+        [DataMember]
+        public string Tekst { get; set; }
+
+    }
+}
---
+++ PorukaServis/PorukaServis/PorukaServis.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace PorukaServis
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class PorukaServis : IPoruka
+    {
+        List<Poruka> listaPoruka = new List<Poruka>()
+        {
+            new Poruka(1, "Ja", "Ti", "Helooou"),
+            new Poruka(2, "Ti", "Ja", "Šta helooou?"),
+            new Poruka(3, "Ja", "Ti", "Što se radi?"),
+            new Poruka(4, "Ti", "Ja", "Uči se DOTN!!!"),
+        };
+        public Poruka JednaPoruka(string id)
+        {
+            int broj;
+
+            if(Int32.TryParse(id, out broj))
+            {
+                // Pronađi element u listi
+                return listaPoruka.First<Poruka>(poruka => poruka.Id == broj);
+            } else
+            {
+                return null; // nije broj
+            }
+        }
+
+        public List<Poruka> SvePoruke()
+        {
+            return listaPoruka;
+        }
+    }
+}
